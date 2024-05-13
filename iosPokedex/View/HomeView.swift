66f46e35882490ec4ad14a2701af8659244b8a96
Mark Allen <markallenarchviz@gmail.com>
2024-05-13 14:58:29 -0300

    feat: use grid to show data

--- iosPokedex/View/HomeView.swift
+++ iosPokedex/View/HomeView.swift
@@ -10,31 +10,42 @@ import SwiftUI
 struct HomeView: View {
     @State var pokemon = [PokemonEntry]()
     @State var searchText = ""
+    
+    // Define the grid columns
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
     var body: some View {
         NavigationView {
-            List {
-                ForEach(searchText == "" ? pokemon : pokemon.filter({
-                    $0.name.contains(searchText.lowercased())
-                })) { entry in
-                    HStack {
-                        PokemonImage(imageLink: "\(entry.url)")
-                            .padding(.trailing, 20)
-                        NavigationLink("\(entry.name)".capitalized,
-                                       destination: Text("Detail view for \(entry.name)"))
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 0) {
+                    ForEach(searchText == "" ? pokemon : pokemon.filter({
+                        $0.name.contains(searchText.lowercased())
+                    })) { entry in
+                        NavigationLink(destination: Text("Detail view for \(entry.name)")) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 15)
+                                    .fill(Color.gray.opacity(0.2))
+                                    .frame(height: 120)
+                                VStack {
+                                    PokemonImage(imageLink: "\(entry.url)")
+                                    Text(entry.name.capitalized)
+                                }
+                                .padding()
+                            }
+                        }
+                        .buttonStyle(PlainButtonStyle())
                     }
-                    
                 }
+                .padding()
             }
             .onAppear {
-                PokeApi().getData() {
-                    pokemon in
+                PokeApi().getData() { pokemon in
                     self.pokemon = pokemon
-                    
-                   
                 }
             }
             .searchable(text: $searchText)
             .navigationTitle("Pokedex")
+            .foregroundColor(.black)
         }
     }
 }
