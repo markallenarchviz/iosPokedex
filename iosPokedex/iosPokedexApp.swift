c920966a51f695c4ca6cf7359d4050ae37a7761a
Mark Allen <markallenarchviz@gmail.com>
2024-05-13 13:00:47 -0300

    init project

---
+++ iosPokedex/iosPokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosPokedexApp.swift
+//  iosPokedex
+//
+//  Created by mark on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iosPokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
